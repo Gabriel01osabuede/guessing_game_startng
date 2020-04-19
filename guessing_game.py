8da8806aaa254115d6dea30164cde3d48a44b3a1
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2020-04-19 09:49:23 +0100

    game

--- guessing_game.py
+++ guessing_game.py
@@ -1,23 +1,27 @@
 # building a guessiing game
+# import the random module  3333
 import random
-
+# printing and introductry message to the game
 print('Welcome to the guessing game')
 print('There are 3 levels to the guessing game'
       '\nStage 1 Easy Level\nStage 2 Medium Level'
       '\nStage 3 Hard level\nPick One Option(stage : 1, 2 or 3)')
 
-
+# functin one for the easy level
 def easylevel():
     chances = 6
     number = 10
     mix = random.randint(1,number)
+    # introductry message for one
     print('Welcome to the Easy Level you have 6 guess chances.'
           '\nGuess the correct Number from 1-10'
           '\nIf you uses all 6 chances with out getting the right answer you loose'
           '\nIf you get the correct answer you move on to Medium level'
           '\nLET PLAY.')
+    # using a while loop to make the game keep going if a wrong guess is given by the user
     while True:
         guess = int(input('Enter Your Answer : '))
+        # using conditions to check the guess of the player
         if guess == mix:
             print('Congratulations, {} was the correct answer '.format(guess))
             print('Next level')
@@ -25,7 +29,7 @@ def easylevel():
         else:
             print('{} Was The Wrong Answer\nTry Again.'.format(guess))
             chances -= 1
-            if chances == 1 or chances == 0:
+            if chances == 1:
                 print('You have {} chance left'.format(chances))
             elif chances == 0:
                 print('Chances exhausted Try again next time.\nGame OVer.')
@@ -39,7 +43,7 @@ def mediumlevel():
     chances = 4
     number = 20
     mix = random.randint(1,number)
-    print('Welcome to the Easy Level you have 4 guess chances.'
+    print('Welcome to the Medium Level you have 4 guess chances.'
           '\nGuess the correct Number from 1-20'
           '\nIf you uses all 4 chances with out getting the right answer you loose'
           '\nIf you get the correct answer you move on to Hard level'
@@ -53,7 +57,7 @@ def mediumlevel():
         else:
             print('{} Was The Wrong Answer\nTry Again.'.format(guess))
             chances -= 1
-            if chances == 1 or chances == 0:
+            if chances == 1:
                 print('You have {} chance left'.format(chances))
             elif chances == 0:
                 print('Chances exhausted Try again next time.\nGame OVer.')
@@ -64,7 +68,7 @@ def hardlevel():
     chances = 3
     number = 50
     mix = random.randint(1,number)
-    print('Welcome to the Easy Level you have 3 guess chances.'
+    print('Welcome to the Hard Level you have 3 guess chances.'
           '\nGuess the correct Number from 1-50'
           '\nIf you uses all 3 chances with out getting the right answer you loose'
           '\nIf you get the correct answer you Win.'
@@ -77,7 +81,7 @@ def hardlevel():
         else:
             print('{} Was The Wrong Answer\nTry Again.'.format(guess))
             chances -= 1
-            if chances == 1 or chances == 0:
+            if chances == 1:
                 print('You have {} chance left'.format(chances))
             elif chances == 0:
                 print('Chances exhausted Try again next time.\nGame OVer.')
