39f539922a9e6bbafa625db50b11011021fff083
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2020-04-21 10:52:11 +0100

    guessing-game

--- guessing_game.py
+++ guessing_game.py
@@ -8,14 +8,13 @@ print('There are 3 levels to the guessing game'
       '\nStage 1 Easy Level\nStage 2 Medium Level'
       '\nStage 3 Hard level\nPick One Option(stage : 1, 2 or 3)')
 
-
 # functin one for the easy level
 def easylevel():
     chances = 6
     number = 10
     mix = random.randint(1,number)
     # introductry message for one
-    print('Welcome to the Easy Level you have 6 guess chances.'
+    print('\nWelcome to the Easy Level you have 6 guess chances.'
           '\nGuess the correct Number from 1-10'
           '\nIf you uses all 6 chances with out getting the right answer you loose'
           '\nIf you get the correct answer you move on to Medium level'
@@ -45,7 +44,7 @@ def mediumlevel():
     chances = 4
     number = 20
     mix = random.randint(1,number)
-    print('Welcome to the Medium Level you have 4 guess chances.'
+    print('\nWelcome to the Medium Level you have 4 guess chances.'
           '\nGuess the correct Number from 1-20'
           '\nIf you uses all 4 chances with out getting the right answer you loose'
           '\nIf you get the correct answer you move on to Hard level'
@@ -72,7 +71,7 @@ def hardlevel():
     chances = 3
     number = 50
     mix = random.randint(1,number)
-    print('Welcome to the Hard Level you have 3 guess chances.'
+    print('\nWelcome to the Hard Level you have 3 guess chances.'
           '\nGuess the correct Number from 1-50'
           '\nIf you uses all 3 chances with out getting the right answer you loose'
           '\nIf you get the correct answer you Win.'
@@ -93,12 +92,15 @@ def hardlevel():
             else:
                 print('You have {} chances left'.format(chances))
 
-selection = int(input('Enter Selection :  '))
-if selection == 1:
-    easylevel()
-elif selection == 2:
-    mediumlevel()
-elif selection == 3:
-    hardlevel()
-else:
-    print('Invalid Selection Try Again')
+try:
+    selection = int(input('Enter Selection :  '))
+    if selection == 1:
+        easylevel()
+    elif selection == 2:
+        mediumlevel()
+    elif selection == 3:
+        hardlevel()
+    elif selection != 1 and selection != 2 and selection != 3:
+        print('Wrong Selection.Try Again\n')
+except ValueError:
+    print('Invalid Selection Try Again\n')
